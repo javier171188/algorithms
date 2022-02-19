bf8e17bda9082e3da3a8f3403179de799f8b69ff
javier171188 <javier171188@hotmail.com>
2022-02-18 19:46:58 -0600

    Set element in exercise 17 seems to be working

--- A_arrays_stacks_queues/exercise17/exercise17.js
+++ A_arrays_stacks_queues/exercise17/exercise17.js
@@ -2,13 +2,39 @@
 //https://www.geeksforgeeks.org/efficient-method-to-store-a-lower-triangular-matrix-using-row-major-mapping/
 
 class triangularArray {
-    constructor(rows, columns) {
-        //TODO: set the correct dimension
-        this.elements = new Array(rows * columns);
+    constructor(dimension) {
+        //TODO: check dim
+        this.dimension = dimension;
+        this.elements = new Array(dimension);
     }
 
-    set(row, colum) {
-        let index = 
+    set(r, c, value) {
+        if (c > r) [c, r] = [r, c];
+        let index = r * (r + 1) / 2 + c;
+
+        if (index > this.dimension - 1) {
+            console.log('Out of borders');
+            return;
+        }
+        this.elements[index] = value;
+    }
+
+    get(r, c) {
+        return this.elements[index];
     }
 
 }
+
+const data = new triangularArray(10);
+data.set(0, 0, 1);
+data.set(1, 0, 2);
+data.set(1, 1, 3);
+data.set(2, 0, 4);
+data.set(2, 1, 5);
+data.set(2, 2, 6);
+data.set(3, 0, 7);
+data.set(1, 3, 8);
+data.set(3, 2, 9);
+data.set(3, 3, 10);
+
+console.log(data);
