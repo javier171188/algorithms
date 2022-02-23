3dccbe9e3fa8eb3954c0b1cdb1fbd285e2875faa
javier171188 <javier171188@hotmail.com>
2022-02-23 13:29:12 -0600

    Exercise 9 seems to be corrected

--- A_arrays_stacks_queues/exercise9/exercise9.js
+++ A_arrays_stacks_queues/exercise9/exercise9.js
@@ -5,7 +5,6 @@ function swapArray(array) {
     const size = array.length;
 
     for (let i = 0; i < size; i++) {
-        console.log(array[array[i]], array[array[i]] % size);
         array[i] += (array[array[i]] % size) * size;
     }
     for (let i = 0; i < size; i++) {
