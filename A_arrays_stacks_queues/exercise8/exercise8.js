b78a412055e26e587fb995948fb6e7d15e8d7ffb
javier171188 <javier171188@hotmail.com>
2022-02-23 13:22:46 -0600

    Exercise 8 seems to be corrected

--- A_arrays_stacks_queues/exercise8/exercise8.js
+++ A_arrays_stacks_queues/exercise8/exercise8.js
@@ -1,16 +1,25 @@
 'use strict';
 
 function sumArrays(array1, array2) {
-    let string1 = array1.join('');
-    let number1 = parseInt(string1);
+    let remaining = 0;
+    let size = Math.max(array1.length, array2.length);
 
-    let string2 = array2.join('');
-    let number2 = parseInt(string2);
+    array1 = Array(size - array1.length).fill(0).concat(array1);
+    array2 = Array(size - array2.length).fill(0).concat(array2);
 
-    let totalInt = number1 + number2;
-    let totalStr = String(totalInt);
-    let totalArray = Array.from(totalStr);
-    return totalArray.map(x => parseInt(x));
+    let result = Array(size);
+    for (let i = size - 1; i >= 0; i--) {
+        result[i] = (array1[i] + array2[i] + remaining) % 10;
+        if (array1[i] + array2[i] + remaining > 9) {
+            remaining = 1;
+        } else {
+            remaining = 0;
+        }
+    }
+    if (remaining === 1) {
+        result = [1].concat(result);
+    }
+    return result;
 }
 
 module.exports = { sumArrays };
