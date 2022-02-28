015cea9f589d2c2ad29fbec55726afa868dda236
javier171188 <javier171188@hotmail.com>
2022-02-28 14:10:09 -0600

    Exercise B3 seems to be working

--- B_strings/exerciseB3/exerciseB3.unit.test.js
+++ B_strings/exerciseB3/exerciseB3.unit.test.js
@@ -1,7 +1,52 @@
 'use strict';
 //https://www.sightwordsgame.com/vocabulary-words/word-play/anagrams/
+const { findAnagram } = require('./exerciseB3');
 
+test('Basic tests', () => {
+    let word = 'melon';
+    let anagram = findAnagram(word);
+    expect(anagram).toEqual('lemon');
 
-test('Basic test', () => {
+    word = 'dense';
+    anagram = findAnagram(word);
+    expect(anagram).toEqual('needs');
 
+    word = 'fsalkda';
+    anagram = findAnagram(word);
+    expect(anagram).toEqual(undefined);
+
+})
+
+test('Check symmetry', () => {
+    let word = 'melon';
+    let anagram = findAnagram(word);
+    expect(anagram).toEqual('lemon');
+
+    word = 'lemon';
+    anagram = findAnagram(word);
+    expect(anagram).toEqual('melon');
+})
+
+test('Short strings', () => {
+    let word = '';
+    let anagram = findAnagram(word);
+    expect(anagram).toEqual(undefined);
+
+    word = 'a';
+    anagram = findAnagram(word);
+    expect(anagram).toEqual(undefined);
+
+    word = 'on';
+    anagram = findAnagram(word);
+    expect(anagram).toEqual('no');
+})
+
+test('Capital letters', () => {
+    let word = "Mars";
+    let anagram = findAnagram(word);
+    expect(anagram).toEqual('rams');
+
+    word = "ocEaN";
+    anagram = findAnagram(word);
+    expect(anagram).toEqual('canoe');
 })
