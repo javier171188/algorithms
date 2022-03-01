536bd550eb7a39c07c60c1ffb9778c25619bd1f0
javier171188 <javier171188@hotmail.com>
2022-03-01 16:39:42 -0600

    Exercise C32 seems to be done

---
+++ C_linked_lists/exerciseC32/exerciseC32.js
@@ -0,0 +1,29 @@
+'use strict';
+//16 full curse
+//https://www.geeksforgeeks.org/find-first-node-of-loop-in-a-linked-list/
+
+function findLoopNode(head) {
+    if (!head || !head.next || !head.next.next) {
+        return null;
+    }
+
+    let slow = head;
+    let fast = head;
+
+    while (fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            break;
+        }
+    }
+    if (!fast.next) return null
+    slow = head;
+    while (slow !== fast) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+    return fast;
+}
+
+module.exports = { findLoopNode };
