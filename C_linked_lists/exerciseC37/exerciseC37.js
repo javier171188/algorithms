af278636f897ea02432c0cbaec6d4251e213a883
javier171188 <javier171188@hotmail.com>
2022-03-03 13:30:49 -0600

    Enqueue seems to be working.

--- C_linked_lists/exerciseC37/exerciseC37.js
+++ C_linked_lists/exerciseC37/exerciseC37.js
@@ -1,4 +1,7 @@
 'use strict';
+
+const { getLinkedListValues } = require('../linkedList');
+
 class Node {
     constructor(val, priority) {
         this.value = val;
@@ -17,9 +20,52 @@ class PriorityQueue {
             this.head.priority = priority;
             return;
         }
+        let newNode = new Node(value, priority);
+        if (!this.head.next) {
+            if (priority > this.head.priority) {
+                newNode.next = this.head;
+                this.head = newNode;
+            } else {
+                this.head.next = newNode;
+            }
+            return;
+        }
+        if (this.head.priority < priority) {
+            newNode.next = this.head;
+            this.head = newNode;
+            return;
+        }
         let currentNode = this.head;
-
-
+        let previousNode;
+        while (currentNode) {
+            if (priority > currentNode.priority) {
+                newNode.next = currentNode;
+                if (previousNode) {
+                    previousNode.next = newNode;
+                }
+                return;
+            }
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+        if (currentNode) {
+            currentNode.next = newNode;
+        } else {
+            previousNode.next = newNode;
+        }
+        return;
     }
-
 }
+
+const priorityQueue = new PriorityQueue();
+priorityQueue.enqueue('A', 1);
+let values = getLinkedListValues(priorityQueue.head);
+//console.log(values);
+
+priorityQueue.enqueue('B', 2);
+values = getLinkedListValues(priorityQueue.head);
+//console.log(values);
+
+priorityQueue.enqueue('C', 1);
+values = getLinkedListValues(priorityQueue.head);
+console.log(values);
