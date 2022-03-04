f06e66c1e9e069b8a878f5a042b6239af7a7c4b0
javier171188 <javier171188@hotmail.com>
2022-03-03 18:56:29 -0600

    Workin on exercise D39

---
+++ D_search_sort/exerciseD39/exerciseD39.js
@@ -0,0 +1,11 @@
+'use strict';
+const values = [2, 5, 8, 4, 10, 3, 9, 7, 2, 4];
+
+const secondLargest = findSecondLargest(values);
+
+function findSecondLargest(values) {
+    for (let v of values) {
+        console.log(v);
+    }
+
+}
