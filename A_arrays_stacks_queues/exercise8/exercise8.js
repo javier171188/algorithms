bf9190c176319a17b5fc825a8c8162cf762757a3
javier171188 <javier171188@hotmail.com>
2022-03-04 13:01:07 -0600

    Exercise A8 seems to be fixed

--- A_arrays_stacks_queues/exercise8/exercise8.js
+++ A_arrays_stacks_queues/exercise8/exercise8.js
@@ -1,24 +1,19 @@
 'use strict';
 
 function sumArrays(array1, array2) {
-    let remaining = 0;
+    let currentValue = 0;
     let size = Math.max(array1.length, array2.length);
 
-    array1 = Array(size - array1.length).fill(0).concat(array1);
-    array2 = Array(size - array2.length).fill(0).concat(array2);
-
     let result = Array(size);
-    for (let i = size - 1; i >= 0; i--) {
-        result[i] = (array1[i] + array2[i] + remaining) % 10;
-        if (array1[i] + array2[i] + remaining > 9) {
-            remaining = 1;
-        } else {
-            remaining = 0;
-        }
+    for (let i = 0; i < size; i++) {
+        currentValue = currentValue + (array1[array1.length - 1 - i] || 0) + (array2[array2.length - 1 - i] || 0);
+        result[size - 1 - i] = (currentValue) % 10;
+        currentValue = Math.floor(currentValue / 10);
     }
-    if (remaining === 1) {
+    if (currentValue === 1) {
         result = [1].concat(result);
     }
+
     return result;
 }
 
--- A_arrays_stacks_queues/exercise8/exercise8.unit.test.js
+++ A_arrays_stacks_queues/exercise8/exercise8.unit.test.js
@@ -1,43 +1,55 @@
 'use strict';
-
 const { sumArrays } = require('./exercise8');
 
-test('Basic tests', () => {
+test('Check example arrays', () => {
     let a = [9, 9];
     let b = [1];
     let o = sumArrays(a, b);
     expect(o).toEqual([1, 0, 0]);
+})
 
-    b = [9, 9];
-    a = [1];
-    o = sumArrays(a, b);
-    expect(o).toEqual([1, 0, 0]);
+describe('Arrays zero', () => {
+    test('The second array is zero', () => {
+        let a = [1, 2, 3, 4, 5, 6, 7];
+        let b = [0];
+        let o = sumArrays(a, b);
+        expect(o).toEqual([1, 2, 3, 4, 5, 6, 7])
+    })
+    test('The first array is zero', () => {
+        let a = [0];
+        let b = [1, 2, 3, 4, 5, 6, 7];
+        let o = sumArrays(a, b);
+        expect(o).toEqual([1, 2, 3, 4, 5, 6, 7])
+    })
+})
 
-    a = [1, 2, 3, 4, 5, 6, 7];
-    b = [8, 9, 1, 2];
-    o = sumArrays(a, b);
-    expect(o).toEqual([1, 2, 4, 3, 4, 7, 9]);
+describe('Arrays of the same size', () => {
+    test('One size arrays', () => {
+        let a = [7];
+        let b = [2];
+        let o = sumArrays(a, b);
+        expect(o).toEqual([9]);
+    })
+    test('Four size arrays', () => {
+        let a = [1, 2, 3, 4];
+        let b = [8, 7, 6, 5];
+        let o = sumArrays(a, b);
+        expect(o).toEqual([9, 9, 9, 9]);
+    })
+})
 
-    b = [0]
-    o = sumArrays(a, b);
-    expect(o).toEqual([1, 2, 3, 4, 5, 6, 7])
 
-    a = [0];
-    o = sumArrays(a, b);
-    expect(o).toEqual([0]);
-
-    a = [7];
-    b = [2];
-    o = sumArrays(a, b);
-    expect(o).toEqual([9]);
-
-    a = [1, 2, 3, 4];
-    b = [8, 7, 6, 5];
-    o = sumArrays(a, b);
-    expect(o).toEqual([9, 9, 9, 9]);
-
-    a = [9, 9, 9, 9, 9, 9, 9, 9];
-    b = [0, 2];
-    o = sumArrays(a, b);
-    expect(o).toEqual([1, 0, 0, 0, 0, 0, 0, 0, 1]);
+describe('Arrays of different size', () => {
+    test('The first array is larger than the second one', () => {
+        let a = [9, 9, 9, 9, 9, 9, 9, 9];
+        let b = [0, 2];
+        let o = sumArrays(a, b);
+        expect(o).toEqual([1, 0, 0, 0, 0, 0, 0, 0, 1]);
+    })
+    test('The second array is larger than the first one', () => {
+        let a = [0, 2];
+        let b = [9, 9, 9, 9, 9, 9, 9, 9];
+        let o = sumArrays(a, b);
+        expect(o).toEqual([1, 0, 0, 0, 0, 0, 0, 0, 1]);
+    })
 })
