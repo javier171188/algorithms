54bd0bdb2266c119f753524297f967c9160afbfd
javier171188 <javier171188@hotmail.com>
2022-03-07 13:20:37 -0600

    Exercise B5 seems to be fixed

--- B_strings/exerciseB5/exerciseB5.js
+++ B_strings/exerciseB5/exerciseB5.js
@@ -2,32 +2,17 @@
 
 function isPermutation(string1, string2) {
     if (string1.length < 1 && string2.length < 1) return true;
-    if (string2.length < 1) return false;
+    if (string1.length !== string2.length) return false;
     if (string1.length === 1 && string2.length === 1) return string1 === string2;
 
-    let permutation = '';
-    let permuted = false;
-
-    getPermutations(string1);
-
-    return permuted;
-    function getPermutations(word) {
-        if (word.length < 2) return;
-        if (word.length === 2) {
-            if (permutation + word[0] + word[1] === string2 ||
-                permutation + word[1] + word[0] === string2) {
-                permuted = true;
-            }
+    for (let l of string1) {
+        let index = string2.indexOf(l);
+        if (index < 0) {
+            return false;
         }
-
-        for (let i = 0; i < word.length; i++) {
-            let tempPerm = permutation;
-            permutation += word[i];
-            getPermutations(word.slice(0, i) + word.slice(i + 1,));
-            permutation = tempPerm;
-            if (permuted) return;
-        }
+        string2 = string2.slice(0, index) + string2.slice(index + 1,);
     }
+    return true;
 }
 
 module.exports = isPermutation; 
--- B_strings/exerciseB5/exerciseB5.unit.test.js
+++ B_strings/exerciseB5/exerciseB5.unit.test.js
@@ -2,91 +2,139 @@
 
 const isPermutation = require('./exerciseB5');
 
-test('Basic tests', () => {
-    let string1 = 'care';
-    let string2 = 'race';
-    let isAPermutation = isPermutation(string1, string2);
-    expect(isAPermutation).toEqual(true);
+describe('Basic tests', () => {
+    test('One is permutation of the other', () => {
+        const string1 = 'care';
+        const string2 = 'race';
+        const isAPermutation = isPermutation(string1, string2);
+        expect(isAPermutation).toEqual(true);
+    })
 
-    string2 = 'another';
-    isAPermutation = isPermutation(string1, string2);
-    expect(isAPermutation).toEqual(false);
+    test('No permutation', () => {
+        const string1 = 'care';
+        const string2 = 'another';
+        const isAPermutation = isPermutation(string1, string2);
+        expect(isAPermutation).toEqual(false);
+    })
 })
 
-test('Check symmetry', () => {
-    let string1 = 'race';
-    let string2 = 'race';
-    let isAPermutation = isPermutation(string1, string2);
-    expect(isAPermutation).toEqual(true);
+describe('Check symmetry', () => {
+    test('The same string', () => {
+        const string1 = 'race';
+        const string2 = 'race';
+        const isAPermutation = isPermutation(string1, string2);
+        expect(isAPermutation).toEqual(true);
+    })
 
-    string2 = 'aecr';
-    isAPermutation = isPermutation(string1, string2);
-    expect(isAPermutation).toEqual(true);
-
-    string1 = 'aecr';
-    string2 = 'race';
-    isAPermutation = isPermutation(string1, string2);
-    expect(isAPermutation).toEqual(true);
+    test('Different order', () => {
+        const string1 = 'race';
+        const string2 = 'aecr';
+        const isAPermutation1 = isPermutation(string1, string2);
+        const isAPermutation2 = isPermutation(string2, string1);
+        const expected = [isAPermutation1, isAPermutation2];
+        expect(expected).toEqual([true, true]);
+    })
 })
 
-test('Short strings', () => {
-    let string1 = '';
-    let string2 = '';
-    let isAPermutation = isPermutation(string1, string2);
-    expect(isAPermutation).toEqual(true);
+describe('Short strings', () => {
+    describe('Empty string', () => {
+        test('Both strings are empty', () => {
+            const string1 = '';
+            const string2 = '';
+            const isAPermutation = isPermutation(string1, string2);
+            expect(isAPermutation).toEqual(true);
+        })
 
-    string2 = 'New string';
-    isAPermutation = isPermutation(string1, string2);
-    expect(isAPermutation).toEqual(false);
+        test('First string is empty', () => {
+            const string1 = '';
+            const string2 = 'New string';
+            const isAPermutation = isPermutation(string1, string2);
+            expect(isAPermutation).toEqual(false);
+        })
 
-    string1 = 'Another string';
-    string2 = '';
-    isAPermutation = isPermutation(string1, string2);
-    expect(isAPermutation).toEqual(false);
+        test('Second string is empty', () => {
+            const string1 = 'Another string';
+            const string2 = '';
+            const isAPermutation = isPermutation(string1, string2);
+            expect(isAPermutation).toEqual(false);
+        })
+    })
 
-    string1 = 'a';
-    string2 = 'b';
-    isAPermutation = isPermutation(string1, string2);
-    expect(isAPermutation).toEqual(false);
+    describe('One dimensional strings', () => {
+        test('Different strings', () => {
+            const string1 = 'a';
+            const string2 = 'b';
+            const isAPermutation = isPermutation(string1, string2);
+            expect(isAPermutation).toEqual(false);
+        })
 
-    string1 = 'a';
-    string2 = 'a';
-    isAPermutation = isPermutation(string1, string2);
-    expect(isAPermutation).toEqual(true);
+        test('The same string', () => {
+            const string1 = 'a';
+            const string2 = 'a';
+            const isAPermutation = isPermutation(string1, string2);
+            expect(isAPermutation).toEqual(true);
+        })
+    })
 
-    string1 = 'ab';
-    string2 = 'ac';
-    isAPermutation = isPermutation(string1, string2);
-    expect(isAPermutation).toEqual(false);
+    describe('Two dimensional strings', () => {
+        test('The strings are not permutation', () => {
+            const string1 = 'ab';
+            const string2 = 'ac';
+            const isAPermutation = isPermutation(string1, string2);
+            expect(isAPermutation).toEqual(false);
+        })
 
-    string2 = 'ab';
-    isAPermutation = isPermutation(string1, string2);
-    expect(isAPermutation).toEqual(true);
+        test('The strings are permutation', () => {
+            const string1 = 'ab';
+            const string2 = 'ba';
+            const isAPermutation = isPermutation(string1, string2);
+            expect(isAPermutation).toEqual(true);
+        })
+    })
 
-    string1 = 'abc';
-    string2 = 'abc';
-    isAPermutation = isPermutation(string1, string2);
-    expect(isAPermutation).toEqual(true);
+    describe('Three dimensional strings', () => {
+        test('The same string', () => {
+            const string1 = 'abc';
+            const string2 = 'abc';
+            const isAPermutation = isPermutation(string1, string2);
+            expect(isAPermutation).toEqual(true);
+        })
 
-    string2 = 'acb';
-    isAPermutation = isPermutation(string1, string2);
-    expect(isAPermutation).toEqual(true);
+        test('No permutation', () => {
+            const string1 = 'abc';
+            const string2 = 'adc';
+            const isAPermutation = isPermutation(string1, string2);
+            expect(isAPermutation).toEqual(false);
+        })
 
-    string2 = 'cba';
-    isAPermutation = isPermutation(string1, string2);
-    expect(isAPermutation).toEqual(true);
+        test('Permutation', () => {
+            const string1 = 'abc';
+            const string2 = 'cba';
+            const isAPermutation = isPermutation(string1, string2);
+            expect(isAPermutation).toEqual(true);
+        })
+    })
+})
 
-    string2 = 'cbaa';
-    isAPermutation = isPermutation(string1, string2);
-    expect(isAPermutation).toEqual(false);
+describe('Different size strings', () => {
+    test('The first is three dimensional and the second four dimensional', () => {
+        const string1 = 'baa';
+        const string2 = 'cbaa';
+        const isAPermutation = isPermutation(string1, string2);
+        expect(isAPermutation).toEqual(false);
+    })
 
-    string1 = 'a';
-    string2 = 'abcde';
-    isAPermutation = isPermutation(string1, string2);
-    expect(isAPermutation).toEqual(false);
+    test('The first is one dimensional and the second is five dimensional', () => {
+        const string1 = 'a';
+        const string2 = 'abcde';
+        const isAPermutation = isPermutation(string1, string2);
+        expect(isAPermutation).toEqual(false);
+    })
 
-    string1 = 'abcde';
-    string2 = 'a';
-    isAPermutation = isPermutation(string1, string2);
-    expect(isAPermutation).toEqual(false);
+    test('The first is four dimensional and the second is one dimensional', () => {
+        const string1 = 'abcde';
+        const string2 = 'a';
+        const isAPermutation = isPermutation(string1, string2);
+        expect(isAPermutation).toEqual(false);
+    })
 })
