360ebe34581525dba04606e872c32561adf6b326
javier171188 <javier171188@hotmail.com>
2022-03-08 11:52:59 -0600

    Finds the largest word, but with repeated letters

--- D_search_sort/exerciseD38/exerciseD38.js
+++ D_search_sort/exerciseD38/exerciseD38.js
@@ -11,26 +11,43 @@ console.log(longest1);
 function getLongestWord(letters) {
     letters = letters.map(l => l.toUpperCase());
 
+    let refLetters = [...letters];
     let proposedWord = '';
     let words = [];
+    let maxWord = '';
+    let currentLetter;
 
     exploreBranch(trieWords, letters);
+    console.log(words);
 
     function exploreBranch(dic, letters) {
         if (Object.keys(dic).length < 1) {
-            console.log(proposedWord);
+            //console.log(proposedWord);
+            words.push(proposedWord);
+            if (proposedWord.length > maxWord.length) maxWord = proposedWord;
             proposedWord = proposedWord.slice(0, -1);
+            //refLetters = [...letters];
+            // console.log(refLetters);
             return;
         }
 
         for (let letter of Object.keys(dic)) {
-            proposedWord += letter;
-            //console.log(proposedWord);
-            exploreBranch(dic[letter], letters)
+            let index = refLetters.indexOf(letter);
+            if (index < 0) {
+                // refLetters.push(currentLetter);
+                // console.log(refLetters);
+                continue;
+            } else {
+                proposedWord += letter;
+                currentLetter = letter;
+                //refLetters.splice(index, 1);
+                // console.log(proposedWord);
+                // console.log(refLetters);
+                exploreBranch(dic[letter], letters);
+            }
         }
         proposedWord = proposedWord.slice(0, -1);
     }
 }
-
 
 module.exports = getLongestWord;
