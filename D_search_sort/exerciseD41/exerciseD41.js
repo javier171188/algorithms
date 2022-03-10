cfd9fbc76553ff55ffcad8dec297c9c5144aa352
javier171188 <javier171188@hotmail.com>
2022-03-10 12:16:37 -0600

    Exercise D41 seems to be done

--- D_search_sort/exerciseD41/exerciseD41.js
+++ D_search_sort/exerciseD41/exerciseD41.js
@@ -1,10 +1,39 @@
 'use strict';
 
-const list = [1, 6, 8, 3, 6, 8, 3];
-const kLargest = getKLargest(list);
-
 function getKLargest(list, k = 1) {
-    if (k < list.length) return undefined;
+    if (list.length < 1) {
+        throw new Error('The list must contain at least one element.');
+    }
+    if (k > list.length) {
+        throw new RangeError('The value of k must be less or equal to the number of elements of the list.');
+    };
+    if (k < 1) {
+        throw new RangeError('The value of k must be greater than zero.');
+    }
 
-
+    const sortedList = mergeSort(list);
+    return sortedList.slice(-k,);
 }
+
+function merge(A, B) {
+    if (A.length < 1) return B;
+    if (B.length < 1) return A;
+
+    if (A[0] < B[0]) {
+        return [A[0]].concat(merge(A.slice(1,), B));
+    }
+    return [B[0]].concat(merge(A, B.slice(1,)));
+}
+
+
+function mergeSort(unsortedArray) {
+    if (unsortedArray.length < 2) return unsortedArray;
+
+    const middleIndex = Math.floor(unsortedArray.length / 2);
+    const leftPart = unsortedArray.slice(0, middleIndex);
+    const rightPart = unsortedArray.slice(middleIndex,);
+    const sortedArray = merge(mergeSort(leftPart), mergeSort(rightPart));
+    return sortedArray;
+}
+
+module.exports = getKLargest;
--- D_search_sort/exerciseD41/exerciseD41.unit.test.js
+++ D_search_sort/exerciseD41/exerciseD41.unit.test.js
@@ -1,5 +1,74 @@
 'use strict';
+const getKLargest = require('./exerciseD41');
 
-test('Basic test', () => {
+test('k greater than the length of the list', () => {
+    const k = 8;
+    const list = [1, 6, 8, 3, 6, 8, 3];
+    function throwingFunction() {
+        getKLargest(list, k);
+    }
+    expect(throwingFunction).toThrow('The value of k must be less or equal to the number of elements of the list.');
+})
 
+test('k is less than 1', () => {
+    const k = 0;
+    const list = [1, 6, 8, 3, 6, 8, 3];
+    function throwingFunction() {
+        getKLargest(list, k);
+    }
+    expect(throwingFunction).toThrow('The value of k must be greater than zero.');
+})
+
+test('k equals 1 must return the maximum', () => {
+    const k = 1;
+    const list = [1, 6, 8, 3, 6, 8, 3];
+    const kLargest = getKLargest(list, k);
+    expect(kLargest).toEqual([8]);
+})
+
+test('k equals length of the list must return whole list values', () => {
+    const k = 7;
+    const list = [1, 6, 8, 3, 6, 8, 3];
+    const kLargest = getKLargest(list, k);
+    expect(kLargest).toEqual([1, 3, 3, 6, 6, 8, 8]);
+})
+
+test('Test k = 3 and list.length = 7', () => {
+    const k = 3;
+    const list = [1, 6, 8, 3, 6, 8, 3];
+    const kLargest = getKLargest(list, k);
+    expect(kLargest).toEqual([6, 8, 8]);
+})
+
+describe('Small arrays', () => {
+    test('Zero sized array', () => {
+        const k = 8;
+        const list = [];
+        function throwingFunction() {
+            getKLargest(list, k);
+        }
+        expect(throwingFunction).toThrow('The list must contain at least one element.');
+    })
+
+    test('One sized array', () => {
+        const k = 1;
+        const list = [8];
+        const kLargest = getKLargest(list, k);
+        expect(kLargest).toEqual([8]);
+    })
+
+    describe('Two sized array', () => {
+        test('k=1', () => {
+            const k = 1;
+            const list = [8, 7];
+            const kLargest = getKLargest(list, k);
+            expect(kLargest).toEqual([8]);
+        })
+        test('k=2', () => {
+            const k = 2;
+            const list = [8, 7];
+            const kLargest = getKLargest(list, k);
+            expect(kLargest).toEqual([7, 8]);
+        })
+    })
 })
