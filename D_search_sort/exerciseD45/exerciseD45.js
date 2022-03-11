b73f9ffa327acad8dcfd0cf188a8f25d39743112
javier171188 <javier171188@hotmail.com>
2022-03-11 17:18:23 -0600

    Exercise D45 seems to be done

--- D_search_sort/exerciseD45/exerciseD45.js
+++ D_search_sort/exerciseD45/exerciseD45.js
@@ -1,9 +1,44 @@
 'use strict';
 
-const number = 45;
-const sequence = findSequence(number);
-console.log(sequence);
-
 function findSequence(target) {
+    if (target === 0) return [target];
+    let positive = true;
+
+    if (target < 0) {
+        positive = false;
+        target = -1 * target;
+    }
+
     const middle = Math.floor(target / 2);
+
+    for (let i = 1; i <= middle; i++) {
+        for (let j = i + 1; j <= middle + 1; j++) {
+            let currentSum = sumInterval(i, j);
+
+            if (currentSum === target && positive) return createSequence(i, j);
+            if (currentSum === target) return createSequence(-1 * j, -1 * i);
+            if (currentSum > target) break;
+        }
+    }
+
+    if (positive) return createSequence(-1 * target + 1, target);
+    return createSequence(-1 * target, target - 1);
 }
+
+function createSequence(start, stop) {
+    let numbers = [];
+    for (let i = start; i <= stop; i++) {
+        numbers.push(i);
+    }
+    return numbers;
+}
+
+function sumInterval(start, stop) {
+    let result = 0;
+    for (let i = start; i <= stop; i++) {
+        result += i;
+    }
+    return result;
+}
+
+module.exports = findSequence;
--- D_search_sort/exerciseD45/exerciseD45.unit.test.js
+++ D_search_sort/exerciseD45/exerciseD45.unit.test.js
@@ -1,5 +1,32 @@
 'use strict';
 
-test('Basic test', () => {
+const findSequence = require('./exerciseD45');
 
-})
+const targets = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+    - 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15, -16, -17, -18, -19, -20, -21, -22, -23, -24];
+function checkConsecutive(sequence) {
+    for (let i = 0; i < sequence.length - 1; i++) {
+        if (sequence[i] + 1 !== sequence[i + 1]) return false;
+    }
+    return true;
+}
+
+
+test.each(targets)(
+    'Check sum, target %i',
+    (target) => {
+        const sequence = findSequence(target);
+        const sumValue = sequence.reduce((a, b) => a + b, 0);
+        expect(sumValue === target).toBe(true);
+    }
+)
+
+test.each(targets)(
+    'Check that the numbers are consecutive, target %i',
+    (target) => {
+        const sequence = findSequence(target);
+        const consecutive = checkConsecutive(sequence);
+        expect(consecutive).toEqual(true);
+    }
+)
