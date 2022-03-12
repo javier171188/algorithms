633a735af2250c52a1c86d1902f81ca7d890bc87
javier171188 <javier171188@hotmail.com>
2022-03-11 19:45:53 -0600

    Working on exercise D48

---
+++ D_search_sort/exerciseD48/exerciseD48.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const S = 'avsfvasbdackldabcd';
+const T = 'abcd';
+
+const subString = findSubstring(S, T);
+
+function findSubstring(S, T) {
+    const minLength = T.length;
+    for (let letter of T) {
+        console.log(letter);
+    }
+
+    return undefined;
+}
