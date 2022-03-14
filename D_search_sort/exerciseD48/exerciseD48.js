7f5a6eed9baae0a48c551f1b31cffe5276b411b7
javier171188 <javier171188@hotmail.com>
2022-03-14 12:01:36 -0600

    ExerciseD48 seems to be working

--- D_search_sort/exerciseD48/exerciseD48.js
+++ D_search_sort/exerciseD48/exerciseD48.js
@@ -1,32 +1,20 @@
 'use strict';
 
-//const S = 'avsfvasbdackldabcd';
-const S = 'ravsfvsbdckldbcd';
-const T = 'abcd';
-
-const subString = findSubstring(S, T);
-console.log(subString);
-
 function findSubstring(S, T) {
     const stringLength = S.length;
     let minLength = T.length;
-    if (stringLength < minLength) return undefined;
+    if (stringLength < minLength || minLength < 1) return undefined;
 
-    //let smallestRun = S.slice(0, minLength);
     while (minLength <= stringLength) {
         for (let i = 0; i <= stringLength - minLength; i++) {
             let subString = S.slice(i, i + minLength);
             let contains = checkContainsAll(subString, T);
-            //console.log(contains);
             if (contains) {
                 return subString;
             }
-            //console.log(subString);
         }
         minLength += 1;
     }
-
-
     return undefined;
 }
 
@@ -37,3 +25,5 @@ function checkContainsAll(subString, T) {
     }
     return true;
 }
+
+module.exports = findSubstring;
--- D_search_sort/exerciseD48/exerciseD48.unit.test.js
+++ D_search_sort/exerciseD48/exerciseD48.unit.test.js
@@ -1,5 +1,116 @@
 'use strict';
+const findSubstring = require('./exerciseD48');
 
-test('Basic test', () => {
+test('Smallest at the beginning', () => {
+    const S = 'adcbvsfvasbdackldabcd';
+    const T = 'abcd';
 
+    const subString = findSubstring(S, T);
+    expect(subString).toEqual('adcb');
+})
+
+test('Smallest in the end', () => {
+    const S = 'ardlcbvsfvasbdqackldyabcd';
+    const T = 'abcd';
+
+    const subString = findSubstring(S, T);
+    expect(subString).toEqual('abcd');
+})
+
+test('Smallest in the middle', () => {
+    const S = 'ardlcbvsfvabdcackldyabtgcd';
+    const T = 'abcd';
+
+    const subString = findSubstring(S, T);
+    expect(subString).toEqual('abdc');
+})
+
+test('No substring', () => {
+    const S = 'kajujhncyhgtr';
+    const T = 'abcd';
+
+    const subString = findSubstring(S, T);
+    expect(subString).toEqual(undefined);
+})
+
+test('Substring equals all the string', () => {
+    const S = 'ajujhnbrqcyhgtrd';
+    const T = 'abcd';
+
+    const subString = findSubstring(S, T);
+    expect(subString).toEqual(S);
+})
+
+test('Letters are separated by other letter', () => {
+    const S = 'cbcvdlckboahgfh';
+    const T = 'abcd';
+
+    const subString = findSubstring(S, T);
+    expect(subString).toEqual('dlckboa');
+})
+
+test('T equals S', () => {
+    const S = 'adsagageklbc';
+    const T = S;
+
+    const subString = findSubstring(S, T);
+    expect(subString).toEqual(T);
+})
+
+test('T equals S in the opposite direction', () => {
+    const S = 'abcde';
+    const T = 'edcba';
+
+    const subString = findSubstring(S, T);
+    expect(subString).toEqual(S);
+})
+
+describe('Small strings', () => {
+    test('T is empty', () => {
+        const S = 'cbcvdlckboahgfh';
+        const T = '';
+
+        const subString = findSubstring(S, T);
+        expect(subString).toEqual(undefined);
+    })
+
+    test('S is empty', () => {
+        const S = '';
+        const T = 'abcd';
+
+        const subString = findSubstring(S, T);
+        expect(subString).toEqual(undefined);
+    })
+
+    test('T and S  are empty', () => {
+        const S = '';
+        const T = '';
+
+        const subString = findSubstring(S, T);
+        expect(subString).toEqual(undefined);
+    })
+
+    test('S smaller than T', () => {
+        const S = 'abc';
+        const T = 'abcd';
+
+        const subString = findSubstring(S, T);
+        expect(subString).toEqual(undefined);
+    })
+
+    test('T is one sized', () => {
+        const S = 'abc';
+        const T = 'c';
+
+        const subString = findSubstring(S, T);
+        expect(subString).toEqual(T);
+    })
+
+    test('T is two sized', () => {
+        const S = 'abc';
+        const T = 'cb';
+
+        const subString = findSubstring(S, T);
+        expect(subString).toEqual('bc');
+    })
 })
