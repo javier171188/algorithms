b4b47aaa8801f49276129dd73d8f49b6ef1e6f06
javier171188 <javier171188@hotmail.com>
2022-03-16 13:13:41 -0600

    Exercise D54 seems to be done

--- E_trees/exerciseD54/exerciseD54.js
+++ E_trees/exerciseD54/exerciseD54.js
@@ -1,7 +1,46 @@
 'use strict';
 
-const dictionary = {};
+class Trie {
+    constructor() {
+        this.root = {};
+    }
+
+    add_word(word, meaning) {
+        let currentNode = this.root;
+        for (let letter of word) {
+            if (!(letter in currentNode)) {
+                currentNode[letter] = {};
+            }
+            currentNode = currentNode[letter];
+        }
+        currentNode['meaning'] = meaning;
+    }
+
+    find_word(word) {
+        let currentNode = this.root;
+
+        for (let letter of word) {
+            if (letter in currentNode) {
+                currentNode = currentNode[letter];
+            } else {
+                return undefined;
+            }
+        }
+        if ('meaning' in currentNode) {
+            return currentNode['meaning'];
+        }
+        return undefined;
+    }
+}
 
 function toTrie(dic) {
+    const trieDic = new Trie();
 
+    for (let word in dic) {
+        trieDic.add_word(word, dic[word]);
+    }
+
+    return trieDic;
 }
+
+module.exports = toTrie;
--- E_trees/exerciseD54/exerciseD54.unit.test.js
+++ E_trees/exerciseD54/exerciseD54.unit.test.js
@@ -1,5 +1,86 @@
 'use strict';
 
-test('Basic test', () => {
+const toTrie = require('./exerciseD54');
 
+describe('Small dictionaries', () => {
+    describe('Empty dictionary', () => {
+        const dictionary = {
+        };
+        const trieDic = toTrie(dictionary);
+
+        test('Check initial content', () => {
+            expect(trieDic.root).toEqual({});
+        })
+
+        test('Check an unknown word', () => {
+            const meaning = trieDic.find_word('easy');
+            expect(meaning).toEqual(undefined);
+        })
+
+        test('Add a word', () => {
+            trieDic.add_word('easy', 'meaning');
+            const meaning = trieDic.find_word('easy');
+            expect(meaning).toEqual('meaning');
+        })
+
+        test('Check unknown word after adding another', () => {
+            trieDic.add_word('word', 'meaning');
+            const meaning = trieDic.find_word('other');
+            expect(meaning).toEqual(undefined);
+        })
+    })
+
+    describe('Dictionary with one word', () => {
+        const dictionary = {
+            easy: 'word 1'
+        };
+        const trieDic = toTrie(dictionary);
+
+        test('Check unknown word', () => {
+            const meaning = trieDic.find_word('other');
+            expect(meaning).toEqual(undefined);
+        })
+
+        test('Check known word', () => {
+            const meaning = trieDic.find_word('easy');
+            expect(meaning).toEqual('word 1');
+        })
+
+        test('Add word', () => {
+            trieDic.add_word('word', 'meaning 2');
+            trieDic.add_word('other', 'meaning 3');
+            trieDic.add_word('more', 'meaning 4');
+            const meaning = trieDic.find_word('other');
+            expect(meaning).toEqual('meaning 3');
+        })
+    })
+
+
+})
+
+describe('Dictionary with three words', () => {
+    const dictionary = {
+        "word1": 'meaning 1',
+        "word2": 'meaning 2',
+        "word3": 'meaning 3'
+    };
+    const trieDic = toTrie(dictionary);
+
+    test('Check unknown word', () => {
+        const meaning = trieDic.find_word('other');
+        expect(meaning).toEqual(undefined);
+    })
+
+    test('Check known word', () => {
+        const meaning = trieDic.find_word('word3');
+        expect(meaning).toEqual('meaning 3');
+    })
+
+    test('Add word', () => {
+        trieDic.add_word('word4', 'meaning 4');
+        trieDic.add_word('word5', 'meaning 5');
+        trieDic.add_word('word6', 'meaning 6');
+        const meaning = trieDic.find_word('word6');
+        expect(meaning).toEqual('meaning 6');
+    })
 })
