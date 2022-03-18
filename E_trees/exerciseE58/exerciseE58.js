faaf6724840e09a5694b8e0908f65bbe63d00e3a
javier171188 <javier171188@hotmail.com>
2022-03-18 16:27:14 -0600

    Searching works on AVL trees, however these do not rebalance yet

--- E_trees/exerciseE58/exerciseE58.js
+++ E_trees/exerciseE58/exerciseE58.js
@@ -1,1 +1,59 @@
 'use strict';
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = undefined;
+        this.right = undefined;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = undefined;
+    }
+
+    insert(value) {
+        if (!this.root) {
+            this.root = new Node(value);
+            return;
+        }
+
+        insert_value(value, this.root);
+
+        function insert_value(value, node) {
+            if (!node) {
+                let newNode = new Node(value);
+                return newNode;
+            }
+
+            if (node.value < value) {
+                //Insert at right
+                node.right = insert_value(value, node.right);
+            } else {
+                //Insert at left
+                node.left = insert_value(value, node.left);
+            }
+            return node;
+        }
+    }
+}
+
+
+function searchInAVLTree(tree, value) {
+    let currentNode = tree.root;
+
+    while (currentNode) {
+        if (currentNode.value === value) return true;
+
+        if (currentNode.value < value) {
+            currentNode = currentNode.right;
+        } else {
+            currentNode = currentNode.left;
+        }
+    }
+
+    return false;
+}
+
+module.exports = { searchInAVLTree, BinarySearchTree };
--- E_trees/exerciseE58/exerciseE58.unit.test.js
+++ E_trees/exerciseE58/exerciseE58.unit.test.js
@@ -1,5 +1,89 @@
 'use strict';
 
-test('Test jest', () => {
+const { searchInAVLTree, BinarySearchTree } = require('./exerciseE58');
 
+test('Search in an empty tree', () => {
+    const avlTree = new BinarySearchTree();
+
+    const contains = searchInAVLTree(avlTree, 10);
+    expect(contains).toEqual(false);
+})
+
+describe('Search in one node', () => {
+    const avlTree = new BinarySearchTree();
+    avlTree.insert(10);
+    test('No contains the value', () => {
+        const contains = searchInAVLTree(avlTree, 20);
+        expect(contains).toEqual(false);
+    })
+
+    test('Contains the value', () => {
+        const contains = searchInAVLTree(avlTree, 10);
+        expect(contains).toEqual(true);
+    })
+})
+
+describe('Tree 10-1-8-20-15-30', () => {
+    const avlTree = new BinarySearchTree();
+    avlTree.insert(10);
+    avlTree.insert(1);
+    avlTree.insert(8);
+    avlTree.insert(20);
+    avlTree.insert(15);
+    avlTree.insert(30);
+    describe('Contain the searched value', () => {
+        test('Value: 10', () => {
+            const contains = searchInAVLTree(avlTree, 10);
+            expect(contains).toEqual(true);
+        })
+        test('Value: 1', () => {
+            const contains = searchInAVLTree(avlTree, 1);
+            expect(contains).toEqual(true);
+        })
+        test('Value: 8', () => {
+            const contains = searchInAVLTree(avlTree, 8);
+            expect(contains).toEqual(true);
+        })
+        test('Value: 20', () => {
+            const contains = searchInAVLTree(avlTree, 20);
+            expect(contains).toEqual(true);
+        })
+        test('Value: 15', () => {
+            const contains = searchInAVLTree(avlTree, 15);
+            expect(contains).toEqual(true);
+        })
+        test('Value: 30', () => {
+            const contains = searchInAVLTree(avlTree, 30);
+            expect(contains).toEqual(true);
+        })
+    })
+
+    describe('Not contain the searched value', () => {
+        test('Value: 2', () => {
+            const contains = searchInAVLTree(avlTree, 2);
+            expect(contains).toEqual(false);
+        })
+
+        test('Value: 5', () => {
+            const contains = searchInAVLTree(avlTree, 5);
+            expect(contains).toEqual(false);
+        })
+
+        test('Value: 17', () => {
+            const contains = searchInAVLTree(avlTree, 17);
+            expect(contains).toEqual(false);
+        })
+        test('Value: 21', () => {
+            const contains = searchInAVLTree(avlTree, 21);
+            expect(contains).toEqual(false);
+        })
+        test('Value: 40', () => {
+            const contains = searchInAVLTree(avlTree, 40);
+            expect(contains).toEqual(false);
+        })
+        test('Value: 50', () => {
+            const contains = searchInAVLTree(avlTree, 50);
+            expect(contains).toEqual(false);
+        })
+    })
 })
